d322858734c93004666fce82fac4f4683aab24f6
João Leitão <jleitaopv@gmail.com>
2026-03-20 19:57:46 +0000

    Solution to exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
+
+  const reversedString = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
